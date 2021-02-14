bf3ff8c611ff85cd50ed3bbf8018354ca82a5093
Poo-jesh <shettypoojesh@gmail.com>
2021-02-14 17:16:41 +0530

    add thread server

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -2,6 +2,8 @@
 #
 # A *bookmark server* or URI shortener.
 
+import threading
+from socketserver import ThreadingMixIn
 import http.server
 import requests
 import os
@@ -99,5 +101,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     server_address = ('', int(os.environ.get('PORT', '8000')))
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
